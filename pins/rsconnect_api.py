5af4cbca6adb4e68f5370ebbf5a76e4d08955a0c
Michael Chow <mc_al_github@fastmail.com>
2022-02-25 18:50:38 -0500

    feat: rsconnect content deployment with tests

--- pins/rsconnect_api.py
+++ pins/rsconnect_api.py
@@ -1,15 +1,42 @@
 import os
+import json
 import requests
+import tempfile
+
+from dataclasses import dataclass, asdict, field
+from pathlib import Path
+from functools import partial
+from io import IOBase
+
+
+def _download_file(response, local_fname):
+    """Download a potentially large file. Note that this mutates the response.
+
+    Adapted from https://stackoverflow.com/a/39217788
+    """
+
+    import shutil
+
+    response.raw.read = partial(response.raw.read, decode_content=True)
+
+    if isinstance(local_fname, IOBase):
+        shutil.copyfileobj(response.raw, local_fname)
+    else:
+        with open(local_fname, "wb") as f:
+            shutil.copyfileobj(response.raw, f)
 
 
 class RsConnectApiError(Exception):
     pass
 
 
+class RsConnectApiRequestError(RsConnectApiError):
+    pass
+
+
 class RsConnectApi:
     api_key: "str | None"
     server_url: "str"
-    base_v1_url: "str"
 
     def __init__(self, server_url, api_key=None):
         self.server_url = server_url
@@ -18,18 +45,23 @@ class RsConnectApi:
     # utility functions -------------------------------------------------------
 
     @property
-    def base_v1_url(self):
+    def base_v1_url(self) -> str:
         return f"{self.server_url}/__api__/v1"
 
     @staticmethod
-    def _get_params(d):
+    def _get_params(d, exclude: set = None):
         """Helper function to return the arguments passed to a method as a dictionary.
 
         Note that it should be the first thing called, using this format:
         self._get_params(locals())
         """
 
-        kwargs = {k: v for k, v in d.items() if k != "self" if v is not None}
+        if exclude is None:
+            full_exclude = {"self"}
+        else:
+            full_exclude = {"self"} | set(exclude)
+
+        kwargs = {k: v for k, v in d.items() if k not in full_exclude if v is not None}
         return kwargs
 
     def _get_api_key(self):
@@ -42,6 +74,15 @@ class RsConnectApi:
 
     def _get_headers(self):
         return {"Authorization": f"key {self._get_api_key()}"}
+
+    def _validate_json_response(self, data: "dict | list"):
+        if isinstance(data, list):
+            return
+
+        # note that code is 0 for successful tasks
+        code = data.get("code")
+        if code is not None and code != 0:
+            raise RsConnectApiRequestError(data)
 
     def query_v1(self, route, method="GET", return_request=False, **kwargs):
         endpoint = f"{self.base_v1_url}/{route}"
@@ -63,8 +104,10 @@ class RsConnectApi:
 
         if return_request:
             return r
-
-        return r.json()
+        else:
+            data = r.json()
+            self._validate_json_response(data)
+            return data
 
     def walk_paginated_offsets(self, f_query, endpoint, method, params=None, **kwargs):
         if params is None:
@@ -88,12 +131,13 @@ class RsConnectApi:
 
     # endpoints ---------------------------------------------------------------
 
-    def ping(self):
-        # TODO: may not be under __api__ route
-        return self._raw_query(f"{self.server_url}/__ping__")
+    # users ----
 
-    def get_user(self):
-        return self.query_v1("user")
+    def get_user(self, guid: str = None):
+        if guid is None:
+            return self.query_v1("user")
+
+        return self.query_v1(f"user/{guid}")
 
     def get_users(
         self,
@@ -109,18 +153,140 @@ class RsConnectApi:
         params = {k: v for k, v in locals().items() if k != "self" if v is not None}
 
         if walk_pages:
-            return self.walk_paginated_offsets(self.query_v1, "users", "GET")
+            return self.walk_paginated_offsets(self.query_v1, "users", "GET", params)
         else:
             return self.query_v1("users", params=params)
+
+    # content ----
 
     def get_content(self, owner_guid: str = None, name: str = None):
         params = self._get_params(locals())
 
         return self.query_v1("content", params=params)
 
-    def get_content_item(self, guid):
-        # endpoint = f"{self.base_v1_url}/content/{guid}"
-        pass
+    def get_content_item(self, guid: str):
+        return self.query_v1(f"content/{guid}")
+
+    def post_content_item(
+        self, name, access_type, title: str = "", description: str = "", **kwargs
+    ):
+        data = self._get_params(locals(), exclude={"kwargs"})
+        result = self.query_v1("content", "POST", json={**data, **kwargs})
+
+        return result
+
+    def post_content_item_deploy(self, guid: str, bundle_id: "str | None" = None):
+        json = {"bundle_id": bundle_id} if bundle_id is not None else {}
+        return self.query_v1(f"content/{guid}/deploy", "POST", json=json)
+
+    def patch_content_item(self, guid, **kwargs):
+        # see https://docs.rstudio.com/connect/api/#patch-/v1/content/{guid}
+        result = self.query_v1(f"content/{guid}", "PATCH", json=kwargs)
+
+        return result
+
+    def delete_content(self, guid: str):
+        """Delete content.
+
+        Note that this method returns None if successful. Otherwise, it raises an error.
+        """
+
+        # if deletion is sucessful, then it will return an empty body, so we
+        # need to check the response manually.
+        r = self.query_v1(f"content/{guid}", "DELETE", return_request=True)
+
+        try:
+            # if we get json back it should always be an api error
+            data = r.json()
+            self._validate_json_response(data)
+
+            # this should never be triggered
+            raise ValueError(
+                "Unknown json returned by delete_content endpoint: %s" % data
+            )
+        except json.JSONDecodeError:
+            # fallback to at least raising status errors
+            r.raise_for_status()
+
+    # bundles ----
+
+    def get_content_bundles(self, guid: str):
+        return self.query_v1(f"content/{guid}/bundles")
+
+    def get_content_bundle(self, guid: str, id: int):
+        # TODO(question): could combine with get_content_bundles above?
+        return self.query_v1(f"content/{guid}/bundles/{id}")
+
+    def get_content_bundle_archive(self, guid: str, id: str, f_obj: "str | IOBase"):
+
+        r = self.query_v1(
+            f"content/{guid}/bundles/{id}/download", stream=True, return_request=True
+        )
+        _download_file(r, f_obj)
+        r.raise_for_status()
+        return r
+
+    def post_content_bundle(self, guid, fname, gzip=True):
+        route = f"content/{guid}/bundles"
+        f_request = partial(self.query_v1, route, "POST")
+
+        p = Path(fname)
+        if p.is_dir() and gzip:
+            import tarfile
+
+            with tempfile.NamedTemporaryFile(mode="wb", suffix=".tar.gz") as tmp:
+                with tarfile.open(fileobj=tmp.file, mode="w:gz") as tar:
+                    tar.add(str(p.absolute()), arcname="")
+
+                # close the underlying file. note we don't call the top-level
+                # close method, since that would delete the temporary file
+                tmp.file.close()
+
+                with open(tmp.name, "rb") as f:
+                    return f_request(data=f)
+        else:
+            with open(str(p.absolute()), "rb") as f:
+                return f_request(data=f)
+
+    # tasks ----
+
+    def get_tasks(self, id: str, first: int = None, wait: int = None):
+        params = self._get_params(locals())
+        del params["id"]
+
+        return self.query_v1(f"tasks/{id}", params=params)
+
+    def poll_tasks(self, id: str, first: int = None, wait: int = 1):
+        """Poll a task until complete."""
+
+        json = self.get_tasks(id, first, wait)
+        while not json["finished"]:
+            json = self.get_tasks(id, json["last"], wait)
+
+        return json
+
+    # non-api endpointsmisc ----
+
+    def misc_ping(self):
+        # TODO: may not be under __api__ route
+        return self._raw_query(f"{self.server_url}/__ping__")
+
+    def misc_get_content_bundle_file(
+        self, guid: str, id: str, fname: str, f_obj: "str | IOBase | None" = None
+    ):
+        if f_obj is None:
+            f_obj = fname
+
+        route = f"{self.server_url}/content/{guid}/_rev{id}/{fname}"
+
+        r = self._raw_query(route, return_request=True, stream=True)
+        r.raise_for_status()
+
+        _download_file(r, f_obj)
+
+    def misc_get_applications(self, filter: str, count: int = 1000):
+        params = self._get_params()
+        return self._raw_query(f"{self.server_url}/applications", params=params)
 
 
 # ported from github.com/rstudio/connectapi
@@ -165,6 +331,49 @@ class _HackyConnect(RsConnectApi):
         )
 
         return RsConnectApi(self.server_url, api_key=api_key["key"])
+
+
+@dataclass
+class PinBundleManifestMetadata:
+    appmode: str = "static"
+    primary_rmd: "str|None" = None
+    primary_html: str = "index.html"
+    content_category: str = "pin"
+    has_parameters: bool = False
+
+
+@dataclass
+class PinBundleManifest:
+    version: int = 1
+    local: str = "en_US"
+    platform: str = "3.5.1"
+    metadata: PinBundleManifestMetadata = field(
+        default_factory=PinBundleManifestMetadata
+    )
+    packages: None = None
+    files: list = field(default_factory=list)
+    users: None = None
+
+    @classmethod
+    def from_directory(cls, dir_name, recursive: bool = True):
+        root_dir = Path(dir_name)
+
+        paths = root_dir.rglob("*") if recursive else root_dir.glob("*")
+        flat_rel_files = [str(p.relative_to(root_dir)) for p in paths]
+
+        return cls(files=flat_rel_files)
+
+    @classmethod
+    def add_manifest_to_directory(cls, dir_name: "str | Path", **kwargs) -> None:
+        import json
+
+        # TODO(question): R lib uses RSCONNECT_TAR env variable
+        bundle = cls.from_directory(dir_name, **kwargs)
+        with (Path(dir_name) / "manifest.json").open("w") as f_manifest:
+            json.dump(bundle.to_dict(), f_manifest)
+
+    def to_dict(self):
+        return asdict(self)
 
 
 class RsConnectFs:
--- pins/tests/test_rsconnect_api.py
+++ pins/tests/test_rsconnect_api.py
@@ -1,6 +1,15 @@
 import pytest
 import json
-from pins.rsconnect_api import RsConnectApi, RsConnectFs
+import tempfile
+
+from pathlib import Path
+from requests.exceptions import HTTPError
+from pins.rsconnect_api import (
+    RsConnectApi,
+    RsConnectFs,
+    RsConnectApiRequestError,
+    PinBundleManifest,
+)
 
 pytestmark = pytest.mark.rsc  # noqa
 
@@ -9,6 +18,9 @@ RSC_SERVER_URL = "http://localhost:3939"
 # TODO: should use pkg_resources for this path?
 RSC_KEYS_FNAME = "pins/tests/rsconnect_api_keys.json"
 
+N_USERS = 3
+ERROR_CODE_BAD_GUID = 3
+
 
 def rsc_from_key(name):
     with open(RSC_KEYS_FNAME) as f:
@@ -16,9 +28,40 @@ def rsc_from_key(name):
         return RsConnectApi(RSC_SERVER_URL, api_key)
 
 
-@pytest.fixture
-def rs_admin():
-    return rsc_from_key("admin")
+def delete_user_content(rsc):
+    guid = rsc.get_user()["guid"]
+    content = rsc.get_content(owner_guid=guid)
+    for entry in content:
+        rsc.delete_content(entry["guid"])
+
+
+# The two rsconnect sessions below tear down at different intervals.
+
+
+@pytest.fixture(scope="session")
+def rsc_admin():
+    rsc_admin = rsc_from_key("admin")
+
+    yield rsc_admin
+
+    # attempt to clean up. since this uses methods that are tested below, it is
+    # possible for the cleanup to fail. we mitigate the risk of this by ordering
+    # the cleanup methods before any POST methods below. but we need to create
+    # content to test deletion, so hope it works as intended during cleanup.
+    delete_user_content(rsc_admin)
+
+
+@pytest.fixture(scope="function")
+def rsc_short():
+    # tears down content after each test
+    rsc_susan = rsc_from_key("susan")
+
+    # delete any content that might already exist
+    delete_user_content(rsc_susan)
+
+    yield rsc_susan
+
+    delete_user_content(rsc_susan)
 
 
 @pytest.fixture
@@ -28,22 +71,220 @@ def fs_admin():
 
 # RsConnectApi ----------------------------------------------------------------
 
+# Methods used in fixture teardown ----
 
-def test_rsconnect_api_get_user(rs_admin):
-    me = rs_admin.get_user()
+
+def test_rsconnect_api_get_user(rsc_admin):
+    me = rsc_admin.get_user()
     assert me["username"] == "admin"
 
 
-def test_rsconnect_api_ping(rs_admin):
-    d = rs_admin.ping()
+def test_rsconnect_get_content_empty(rsc_short):
+    me = rsc_short.get_user()
+    content = rsc_short.get_content(owner_guid=me["guid"])
+    assert len(content) == 0
+
+
+# User ----
+
+
+def test_rsconnect_api_get_users(rsc_admin):
+    users = rsc_admin.get_users()
+    assert len(users) == N_USERS
+    # sanity check that it's got details about users
+    assert "guid" in users[0].keys()
+
+
+def test_rsconnect_api_get_users_arg_prefix(rsc_admin):
+    # get the user "susan"
+    users = rsc_admin.get_users(prefix="susa")
+    assert len(users) == 1
+    assert users[0]["username"] == "susan"
+
+
+def test_rsconnect_api_get_users_no_pagination(rsc_admin):
+    # walking pages appends all result entries into a single list
+    # so we need to access to the "results" entry.
+    users = rsc_admin.get_users(walk_pages=False)
+    assert len(users["results"]) == N_USERS
+
+    # fields used for pagination cursors
+    assert "current_page" in users
+    assert "total" in users
+
+
+# Content ----
+
+
+def test_rsconnect_api_post_content_item(rsc_short):
+    content = rsc_short.post_content_item("test-content", "acl")
+    assert "guid" in content.keys()
+    assert content["name"] == "test-content"
+
+
+def test_rsconnect_api_post_content_item_fail(rsc_short):
+    with pytest.raises(RsConnectApiRequestError):
+        rsc_short.post_content_item("test bad content", "acl")
+
+
+def test_rsconnect_api_patch_content_item(rsc_short):
+    post_res = rsc_short.post_content_item("test-content", "acl", title="old title")
+    patch_res = rsc_short.patch_content_item(post_res["guid"], title="new title")
+
+    assert post_res["title"] == "old title"
+    assert patch_res["title"] == "new title"
+
+
+def test_rsconnect_get_content_item(rsc_short):
+    post_res = rsc_short.post_content_item("test-content", "acl")
+    get_res = rsc_short.get_content_item(post_res["guid"])
+    assert post_res == get_res
+
+
+def test_rsconnect_get_content_args(rsc_short):
+    me = rsc_short.get_user()
+
+    post_res = rsc_short.post_content_item("test-content", "acl")
+    get_res_all = rsc_short.get_content(me["guid"], post_res["name"])
+
+    assert len(get_res_all) == 1
+    assert post_res == get_res_all[0]
+
+
+def test_rsconnect_del_content_result(rsc_short):
+    post_res = rsc_short.post_content_item("test-content", "acl")
+    res = rsc_short.delete_content(post_res["guid"])
+    assert res is None
+
+
+def test_rsconnect_del_content_fails(rsc_short):
+    rsc_short.post_content_item("test-content", "acl")
+
+    with pytest.raises(RsConnectApiRequestError) as exc_info:
+        rsc_short.delete_content("abc")
+
+    err = exc_info.value.args[0]
+
+    # see https://docs.rstudio.com/connect/api/#overview--api-error-codes
+    assert err["code"] == ERROR_CODE_BAD_GUID
+    assert "abc" in err["error"]
+
+
+# Bundles ----
+
+
+def create_content_bundle(rsc, guid, target_dir=None):
+    # TODO: commit a bundle to this repo, and add another test for that
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_dir = tmp_dir if target_dir is None else target_dir
+        (Path(tmp_dir) / "index.html").write_text("<html><body>yo</body></html>")
+        PinBundleManifest.add_manifest_to_directory(tmp_dir)
+        return rsc.post_content_bundle(guid, tmp_dir)
+
+
+def test_rsconnect_api_post_content_bundle(rsc_short):
+    # create bundle
+    content = rsc_short.post_content_item("test-content-bundle", "acl")
+    post_bundle_res = create_content_bundle(rsc_short, content["guid"])
+
+    assert "content_guid" in post_bundle_res
+    assert "id" in post_bundle_res
+
+
+def test_rsconnect_api_post_content_bundle_fail(rsc_short):
+    # create bundle
+    rsc_short.post_content_item("test-content-bundle", "acl")
+    with pytest.raises(RsConnectApiRequestError) as exc_info:
+        create_content_bundle(rsc_short, "A-BAD-GUID")
+
+    err = exc_info.value.args[0]
+    assert err["code"] == ERROR_CODE_BAD_GUID
+    assert "A-BAD-GUID" in err["error"]
+
+
+def test_rsconnect_api_get_content_bundle(rsc_short):
+    # create bundle
+    content = rsc_short.post_content_item("test-content-bundle", "acl")
+    post_bundle_res = create_content_bundle(rsc_short, content["guid"])
+
+    get_res = rsc_short.get_content_bundle(
+        post_bundle_res["content_guid"], post_bundle_res["id"]
+    )
+
+    assert post_bundle_res == get_res
+
+
+def test_rsconnect_api_get_content_bundle_archive(rsc_short):
+    # TODO: should just use a committed archive
+    import tarfile
+    import filecmp
+    from pathlib import Path
+
+    content = rsc_short.post_content_item("test-content-bundle", "acl")
+    bundle = create_content_bundle(rsc_short, content["guid"])
+
+    # create temporary directories for content source and dest to download to ----
+    with tempfile.TemporaryDirectory() as tmp_src, tempfile.TemporaryDirectory() as tmp_dst:
+        create_content_bundle(rsc_short, content["guid"], tmp_src)
+
+        # download .tar.gz archive to a temporary file and unzip ----
+        with tempfile.NamedTemporaryFile() as tmp_file:
+            rsc_short.get_content_bundle_archive(
+                content["guid"], bundle["id"], tmp_file.name
+            )
+
+            tmp_file.file.close()
+
+            tar = tarfile.open(tmp_file.name, "r:gz")
+            tar.extractall(path=tmp_dst)
+            tar.close()
+
+            # run checks inside context handler for easier de-bugging ----
+            p_src = Path(tmp_src)
+            p_dst = Path(tmp_dst)
+
+            # test that archives both have identical index.html and manifest.json
+            dst_fnames = list(p_dst.glob("*"))
+            assert len(dst_fnames) == 2
+            assert filecmp.cmp(p_src / "index.html", p_dst / "index.html")
+            assert filecmp.cmp(p_src / "manifest.json", p_dst / "manifest.json")
+
+
+# Tasks ----
+
+
+# Misc ----
+
+
+def test_rsconnect_api_ping(rsc_admin):
+    d = rsc_admin.misc_ping()
     assert d == {}
 
 
-def test_rsconnect_api_get_users(rs_admin):
-    users = rs_admin.get_users()
-    assert len(users) == 3
-    # sanity check that it's got details about users
-    assert "guid" in users[0].keys()
+def test_rsconnect_api_misc_get_content_bundle_file(rsc_short):
+    content = rsc_short.post_content_item("test-content-bundle", "acl")
+    bundle = create_content_bundle(rsc_short, content["guid"])
+    rsc_short.post_content_item_deploy(content["guid"], bundle["id"])
+
+    with tempfile.NamedTemporaryFile() as tmp:
+        rsc_short.misc_get_content_bundle_file(
+            content["guid"], bundle["id"], "index.html", tmp.file
+        )
+
+        tmp.file.close()
+        # TODO: could test contents more carefully
+        assert "<html>" in Path(tmp.name).read_text()
+
+
+def test_rsconnect_api_misc_get_content_bundle_file_fail(rsc_short):
+    content = rsc_short.post_content_item("test-content-bundle", "acl")
+    bundle = create_content_bundle(rsc_short, content["guid"])
+
+    # TODO: assert something about result
+    with pytest.raises(HTTPError), tempfile.TemporaryFile() as tmp:
+        rsc_short.misc_get_content_bundle_file(
+            content["guid"], bundle["id"], "NOT-IN-BUNDLE.txt", tmp
+        )
 
 
 # RsConnectFs -----------------------------------------------------------------

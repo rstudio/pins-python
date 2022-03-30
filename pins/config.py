a28072d4796e2c5f2dc3472706d30563a096ad98
Michael Chow <mc_al_github@fastmail.com>
2022-03-30 15:07:58 -0400

    refactor: use pins-py for data and caches for now

--- pins/config.py
+++ pins/config.py
@@ -1,10 +1,12 @@
 import appdirs
 import os
 
+PINS_NAME = "pins-py"
+
 
 def get_data_dir():
-    return os.environ.get("PINS_DATA_DIR", appdirs.user_data_dir("pins"))
+    return os.environ.get("PINS_DATA_DIR", appdirs.user_data_dir(PINS_NAME))
 
 
 def get_cache_dir():
-    return os.environ.get("PINS_CACHE_DIR", appdirs.user_cache_dir("pins"))
+    return os.environ.get("PINS_CACHE_DIR", appdirs.user_cache_dir(PINS_NAME))

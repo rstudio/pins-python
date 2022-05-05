c0e331aa4b1acdef0dd8a2c3489900c9a17adf36
Michael Chow <mc_al_github@fastmail.com>
2022-05-05 10:38:03 -0400

    tests: add pins v0 compat tests

---
+++ pins/tests/test_compat_old_types.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from importlib_resources import files
+
+from pins import board_folder
+
+OLD_BOARD = files("pins") / "tests" / "pins-old-types"
+DST_DF = pd.DataFrame({"a": [1, 2], "b": ["x", "y"]})
+
+
+def test_compat_old_types_load_table():
+    board = board_folder(OLD_BOARD)
+    src_df = board.pin_read("a-table")
+
+    assert isinstance(src_df, pd.DataFrame)
+    assert src_df.equals(DST_DF)

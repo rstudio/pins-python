4dd2274e22af1b8e68c67ef3067910d567d24294
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2024-08-21 16:25:25 +1200

    Disable pyright for protected optional import

--- pins/drivers.py
+++ pins/drivers.py
@@ -122,11 +122,12 @@ def load_data(
 
         elif meta.type == "rds":
             try:
-                import rdata
+                import rdata  # pyright: ignore[reportMissingImport]
             except ModuleNotFoundError:
                 raise ModuleNotFoundError(
                     "Install the 'rdata' package to attempt to convert 'rds' files into Python objects."
                 )
+
             # Equivalent to `rdata.read_rds(f)` but compatible with Python 3.8.
             # See https://github.com/rstudio/pins-python/pull/265
             parsed = rdata.parser.parse_file(f.name)

e6e950e96ad1bb3294effd728e7905ac7d50f523
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-03-30 20:13:43 -0500

    pin_open() works

--- pins/databricks/fs.py
+++ pins/databricks/fs.py
@@ -2,12 +2,15 @@ import os
 from databricks.sdk import WorkspaceClient
 from fsspec import AbstractFileSystem
 from typing import ClassVar
+from io import BytesIO
+import shutil
 
 class DatabricksFs(AbstractFileSystem):
     protocol: ClassVar[str | tuple[str, ...]] = "dbc"
 
     def __init__(self, folder_url, **kwargs):
         self.folder_url = folder_url
+        self.workspace = w = WorkspaceClient()
 
     def ls(self, path, details=False, **kwargs):
         return self._list_folders(path)
@@ -16,9 +19,15 @@ class DatabricksFs(AbstractFileSystem):
         path = os.path.basename(path)
         return path in self._list_folders(self.folder_url)
 
+    def open(self, path: str, mode: str = "rb", *args, **kwargs):
+        resp = self.workspace.files.download(path) 
+        f = BytesIO()
+        shutil.copyfileobj(resp.contents, f)
+        f.seek(0)
+        return f
+
     def _list_folders(self, path):
-        w = WorkspaceClient()
-        dir_contents = list(w.files.list_directory_contents(path))
+        dir_contents = list(self.workspace.files.list_directory_contents(path))
         all_folders = []
         for item in dir_contents:
             if(item.is_directory):
